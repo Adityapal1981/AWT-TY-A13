92b88b4c07ca2c35233ca4879557a999b94b6cb4
Adityapal1981 <aditya1981pal@gmail.com>
2026-07-28 17:52:09 +0530

    Implemented JavaScript validation and DOM manipulation

---
+++ script.js
@@ -0,0 +1,46 @@
+// Task 4: Display Technical Events
+
+var events = ["Hackathon", "Coding Contest", "Robotics Workshop"];
+
+for (var i = 0; i < events.length; i++) {
+    document.getElementById("eventList").innerHTML += "<li>" + events[i] + "</li>";
+}
+
+
+// Task 2 & Task 3
+
+function checkForm() {
+
+    var name = document.getElementById("name").value;
+    var roll = document.getElementById("roll").value;
+    var event = document.getElementById("event").value;
+    var age = document.getElementById("age").value;
+    var attended = document.getElementById("events").value;
+
+    var regex = /^[A-Za-z ]+$/;
+
+    if (name == "" || roll == "" || event == "" || age == "" || attended == "") {
+        alert("Fill all fields");
+        return false;
+    }
+
+    if (regex.test(name) == false) {
+        alert("Enter valid name");
+        return false;
+    }
+
+    if (attended == 0) {
+        document.getElementById("result").innerHTML = "Participation Level: Beginner";
+    }
+    else if (attended <= 3) {
+        document.getElementById("result").innerHTML = "Participation Level: Active Participant";
+    }
+    else if (attended <= 6) {
+        document.getElementById("result").innerHTML = "Participation Level: Enthusiast";
+    }
+    else {
+        document.getElementById("result").innerHTML = "Participation Level: Event Champion";
+    }
+
+    return false;
+}
